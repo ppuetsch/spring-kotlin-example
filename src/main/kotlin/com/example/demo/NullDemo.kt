956551d586e55b7ca7e5510bc041c483d7447248
Philip Puetsch <philip.puetsch@d-fine.com>
2024-09-11 08:47:41 +0200

    some examples

--- src/main/kotlin/com/example/demo/NullDemo.kt
+++ src/main/kotlin/com/example/demo/NullDemo.kt
@@ -1,8 +1,41 @@
-package com.example.demo
-
-class NullDemo {
-    fun NullDemo() {
-        // val, var, Int, Int?
-    }
-
-}
+//package com.example.demo
+//
+//class NullDemo {
+//    fun NullDemo() {
+//        val a: int = 2
+//
+//        // Does not work, as a is val
+//        a=3
+//
+//        var b=5
+//        b=6
+//
+//        // Does not work, as b is not nullable
+//        b=null
+//
+//
+//        var c: Int? = 3
+//        c=null
+//
+//        var classWithMethods: ClassWithMethods? = ClassWithMethods()
+//
+//        //Does not work, as classWithMethods might be null
+//        classWithMethods.getClassWithMethods()
+//
+//        //Safe call: Returns null if the object is null
+//        classWithMethods?.getClassWithMethods(true)?.getClassWithMethods(false)?.getClassWithMethods(false)?.getClassWithMethods(true)
+//
+//        //Using the Not-Null-Assertion-Operator. This way, a Null Pointer Exception is thrown if something is Null
+//        classWithMethods!!.getClassWithMethods(true)!!.getClassWithMethods(false)!!.getClassWithMethods(false)!!.getClassWithMethods(true)
+//
+//
+//    }
+//
+//    class ClassWithMethods{
+//        fun getClassWithMethods(returnNull: Boolean): ClassWithMethods? {
+//
+//            return if (returnNull) null else ClassWithMethods()
+//        }
+//    }
+//
+//}
